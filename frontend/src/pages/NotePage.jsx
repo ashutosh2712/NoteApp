fabfecb812935f389e56560ff98904874c314da9
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-19 15:33:11 +0530

    Implemented the update note feature with ordering by latest

--- frontend/src/pages/NotePage.jsx
+++ frontend/src/pages/NotePage.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams,  } from 'react-router-dom';
 import ArrowLeft from '../assets/leftArrow.png'
 const NotePage = () => { 
 
   const { id } = useParams();
   const noteId = id;
+  const history = useNavigate()
 
   const[note, setNote] = useState(null);
 
@@ -18,14 +19,30 @@ const NotePage = () => {
     setNote(data)
   }
 
+  const updateNote = async () => {
+    fetch(`http://127.0.0.1:8000/api/notes/${noteId}/update/`, {
+          method: "PUT",
+          headers : {
+            'Content-Type' : 'application/json'
+          },
+          body : JSON.stringify(note)
+
+    })
+  }
+
+  let handleSubmit = () => {
+    updateNote()
+    history('/')
+  }
+
   return (
     <div className='note'>
       <div className="note-header">
         <h3>
-          <Link to="/"><img src={ArrowLeft} alt="" /></Link>
+          <Link to="/"><img src={ArrowLeft} alt=""  onClick={handleSubmit}/></Link>
         </h3>
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea onChange={(e) => {setNote({...note, 'body' :e.target.value})}} defaultValue={note?.body}></textarea>
     </div>
   )
 }
